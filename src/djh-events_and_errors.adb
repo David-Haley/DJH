fb87c7fae41f998a8215e363ad41246758dd8dee
David-Haley <david.haley@bigpond.com>
2025-04-11 01:27:46 +1000

    Opening of log files now part of package initialisation

--- src/djh-events_and_errors.adb
+++ src/djh-events_and_errors.adb
@@ -1,7 +1,10 @@
 -- This package manages the event and error logging
 -- Author    : David Haley
 -- Created   : 20/02/2021
--- Last Edit : 23/09/2023
+-- Last Edit : 11/04/2025
+
+-- 20250411 : Open files as package during initialisation, file closeure made
+-- conditional.
 -- 20230923: Removal of redundant spaces from event log messages, error log now
 -- comma delimited.
 -- 20220820: Moved to DJH.Events_and_Errors
@@ -17,24 +20,7 @@ package body DJH.Events_and_Errors is
    Error_File_Name : constant String := "Error_Log.txt";
 
    Event_File, Error_File : File_Type;
-
-   procedure Start_Events is
-
-      -- Opens Event and Error files
-
-   begin -- Start_Events
-      if Exists (Event_File_Name) then
-         Open (Event_File, Append_File, Event_File_Name);
-      else
-         Create (Event_File, Out_File, Event_File_Name);
-      end if; -- Exists (Output_File_Name)
-      if Exists (Error_File_Name) then
-         Open (Error_File, Append_File, Error_File_Name);
-      else
-         Create (Error_File, Out_File, Error_File_Name);
-      end if; -- Exists (Error_File_Name)
-   end Start_Events;
-
+   
    procedure Put_Event (Event_Text : in String) is
 
       -- Writes an event message to Event_Log.txt
@@ -61,8 +47,23 @@ package body DJH.Events_and_Errors is
 
       -- Explicitely closes Event and Error files
    begin -- Stop_Events
-      Close (Event_File);
-      Close (Error_File);
+      if Is_Open (Event_File) then
+         Close (Event_File);
+      end if; -- Is_Open (Event_File)
+      if Is_Open (Error_File) then
+         Close (Error_File);
+      end if; -- Is_Open (Error_File)
    end Stop_Events;
-
+   
+begin -- DJH.Events_and_Errors
+   if Exists (Event_File_Name) then
+      Open (Event_File, Append_File, Event_File_Name);
+   else
+      Create (Event_File, Out_File, Event_File_Name);
+   end if; -- Exists (Output_File_Name)
+   if Exists (Error_File_Name) then
+      Open (Error_File, Append_File, Error_File_Name);
+   else
+      Create (Error_File, Out_File, Error_File_Name);
+   end if; -- Exists (Error_File_Name)
 end DJH.Events_and_Errors;
--- src/djh-events_and_errors.ads
+++ src/djh-events_and_errors.ads
@@ -1,15 +1,15 @@
 -- This package manages the event and error logging
 -- Author    : David Haley
 -- Created   : 20/02/2021
--- Last Edit : 20/08/2022
+-- Last Edit : 11/04/2025
+
+-- 20250411 : Open files as package during initialisation, file closeure made
+-- conditional.
 -- 20220820: Moved to DJH.Events_and_Errors
 
 with Ada.Exceptions; use Ada.Exceptions;
 
 package DJH.Events_and_Errors is
-
-   procedure Start_Events;
-   -- Opens Event and Error files
 
    procedure Put_Event (Event_Text : in String);
    -- Writes an event message to Event_Log.txt
